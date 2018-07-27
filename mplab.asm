b1ba68b76dd80fcd29da9d0dea0675f019040b63
Unknown <azarenko.s@gmail.com>
2018-07-27 17:24:05 +0300

    the first work release

--- mplab.asm
+++ mplab.asm
@@ -1,63 +1,81 @@
 #include p12f675.inc
 
-GPIO_0 equ 0x01
-GPIO_1 equ 0x02
+CNT1 equ 0x20
+CNT2 equ 0x21
 
 rst	org	0x00
-   goto	start
+
+	goto	start
 
 int	org	0x04
-   goto	inthandle
-  
-intvector	org	0x08
-inthandle
-   banksel INTCON 
-	BTFSS INTCON, GPIF ;if it is not a PORT interrupt return
-   retfie
-	banksel GPIO
-	BTFSS GPIO, GPIO_0 ;if it is not a front signal return
 	retfie
+  
+start org 0x20
+	;hardware settings
+    banksel ANSEL
+	movlw 00h 
+	movwf ANSEL
 
-	banksel INTCON 
-	BCF INTCON, GIE ;temporary disable interrupts
+	banksel ADCON0
+	movlw 00h 
+	movwf ADCON0
+
+	banksel CMCON
+	movlw 07h 
+	movwf CMCON
+
+	banksel VRCON
+	movlw 00h 
+	movwf VRCON    
+
+	banksel TRISIO
+	movlw 01h ;set GPIO<0> as input, all others as ouptut
+	movwf TRISIO
+
+	banksel GPIO
+	clrf GPIO
+
+begin   
+	banksel  GPIO
+	BTFSC GPIO, 00h ;if it is not a front signal return
+	goto begin
 	
 	banksel GPIO
-	BSF GPIO, GPIO_1
+	BSF GPIO, 01h
 
-	; active state of injector
-step1
-	MOVLW 0xFF
-	DECFSZ W, 0x01
-	GOTO step1
-step2 
-	MOVLW 0xFF
-	DECFSZ W, 0x01
-	GOTO step2
+	MOVLW 0x05
+	movwf CNT1
 
-	BCF GPIO, GPIO_1
+first_counter
+	DECFSZ CNT1, 1
+	GOTO second_counter
+	GOTO end_of_pulse    
 
-	banksel INTCON
-	BCF INTCON, GPIF
-	BSF INTCON, GIE
-	retfie
+second_counter
+	MOVLW 0x0A
+	movwf CNT2
+	DECFSZ CNT2, 1
+	GOTO $ - 1
+	GOTO first_counter
 
-start
-   ;hardware settings
-   banksel  GPIO
-   clrf GPIO
-   banksel  TRISIO
-   movlw GPIO_0 ;set GPIO<0> as input, GPIO<1> as output
-   movwf TRISIO 
-   
-   ;setup interrupts
-   banksel  IOC   
-   movlw GPIO_0
-   movwf IOC    
-   banksel INTCON 
-   clrf INTCON
-   bsf INTCON, GIE ;Enable Global Int's
-   bsf INTCON, GPIE ;Port Change Interrupt Enable bit
-   
-   ;main loop
-   goto  $
-   end
+end_of_pulse
+	BCF GPIO, 01h
+
+	;pause
+	MOVLW 0x01
+	movwf CNT1
+
+pause_first_counter
+	DECFSZ CNT1, 1
+	GOTO pause_second_counter
+	GOTO begin
+
+pause_second_counter
+	MOVLW 0x01
+	movwf CNT2
+	DECFSZ CNT2, 1
+	GOTO $ - 1
+	GOTO pause_first_counter
+
+	goto begin
+	end
